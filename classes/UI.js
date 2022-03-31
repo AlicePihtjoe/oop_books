b9753b102bffdb20255be6708855cc6d8de43d01
AlicePihtjoe <alice.pihtjoe@khk.ee>
2022-03-31 12:05:39 +0300

    LS data show

--- classes/UI.js
+++ classes/UI.js
@@ -1,28 +1,61 @@
 class UI {
-    addBook(book){
-        // create tr element
-        const tr = document.createElement('tr');
-        // td for title, author, isbn
-        for(let name in book){
-
-            // create to element
-            let to = document.createElement('td');
-            // create text element
-            let text = document.createTextNode(book[name]);
-            // add test to <to>
-            td.appendChild(text);
-            // add to to tr
-            tr.appendChild(td);
-            console.log(tr)
+    // help function to add DOM element
+    addUIElement(name, classname = '', textcontent = '', atributes = {}){
+        // create element
+        const element = document.createElement(name);
+        // add css style class to element
+        if(classname !== ''){
+            element.className = classname
         }
-        let td = document.createElement('td');
-        const link = document.createElement('a');
-        link.setAttribute('href', '#');
-        link.appendChild(document.createTextNode('x'));
-        td.appendChild(link);
-        tr.appendChild(td);
-        const booksList = document.querySelector('#books-list');
-        booksList.appendChild(tr);
+        // add text content to element
+        element.appendChild(document.createTextNode(textcontent))
+        // add atributes to element
+        if(Object.keys(atributes).length > 0){
+            for(let key in atributes){
+                element.setAttribute(key, atributes[key])
+            }
+        }
+        return element
+    }
 
+    addBook(book){
+        // create <tr> element
+        const tr = this.addUIElement('tr')
+        // td for title, author and isbn
+        for(let name in book){
+            // create <td> element and add text
+            let td = this.addUIElement('td', '', book[name])
+            // add td to tr
+            tr.appendChild(td)
+        }
+        // X link
+        // create <td> element
+        let td = this.addUIElement('td')
+        // create <a> element
+        const link = this.addUIElement('a', '', 'X', {'href':'#'})
+        // add link to td
+        td.appendChild(link)
+        // add td to tr
+        tr.appendChild(td)
+        // add tr to tbody
+        const booksList = document.querySelector('#books-list')
+        booksList.appendChild(tr)
+    }
+
+    //if X is clicked, the book is removed from the list
+    delBook(event) {
+        //if X is clicked
+        if(event.target.textContent === 'X') {
+            //ask for confirmation
+            if(confirm('Do you want to delete this book?')){
+                event.target.parentElement.parentElement.remove();
+                let bookISBN = event.target.parentElement.previousElementSibling.textContent;
+                console.log(bookISBN);
+                const ls = new LS();
+                ls.delBook(bookISBN);
+            }
+        }
     }
 }
+
+// comment for the purpose of commiting
